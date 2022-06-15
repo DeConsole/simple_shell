f41030f133cca9057882748356a93b01b228f701
hanichris <hanichris71@gmail.com>
2022-06-15 06:24:29 -0700

    Made syntax changes to align with Betty coding style

--- 0-ProudBetty.c
+++ 0-ProudBetty.c
@@ -1,18 +1,15 @@
-include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - prints the alphabet from a - z 
- * Return: 0 success
- *
+ * main - prints the alphabet from a - z.
+ * Return: 0 success.
  */
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-{
-	putchar(letter);
-}
+		putchar(letter);
 	putchar('\n');
-
 	return (0);
 }
